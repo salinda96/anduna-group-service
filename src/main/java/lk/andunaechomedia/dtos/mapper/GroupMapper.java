87f519317242b0d393bf2b47c7557079ba50961f
sMenaka <dgsmenaka@gmail.com>
2020-08-30 00:45:41 +0530

    start with new

--- src/main/java/lk/andunaechomedia/dtos/mapper/GroupMapper.java
+++ src/main/java/lk/andunaechomedia/dtos/mapper/GroupMapper.java
@@ -3,15 +3,9 @@ package lk.andunaechomedia.dtos.mapper;
 import lk.andunaechomedia.constant.GPSStatus;
 import lk.andunaechomedia.dtos.SaveGroupDto;
 import lk.andunaechomedia.models.DeviceGroup;
-import lk.andunaechomedia.models.TempSchedule;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.sql.Time;
-import java.sql.Timestamp;
+
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.logging.Logger;
-
 public class GroupMapper {
    static public DeviceGroup fromSaveDto(SaveGroupDto groupDto){
 
