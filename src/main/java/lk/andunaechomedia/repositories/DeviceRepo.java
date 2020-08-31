2f18771dd69c78a9499161ca6ced29d0f9bc2b32
tharindudilvan <dilvantharindu@gmail.com>
2020-08-31 14:57:37 +0530

    before pull commit

--- src/main/java/lk/andunaechomedia/repositories/DeviceRepo.java
+++ src/main/java/lk/andunaechomedia/repositories/DeviceRepo.java
@@ -4,9 +4,9 @@ import lk.andunaechomedia.models.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository
 public interface DeviceRepo extends JpaRepository<Device,String> {
-    public List<Device> getAllByGroup_id(String id);
+
 }
